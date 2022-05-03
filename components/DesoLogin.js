94575229d5fc7239aa746ec1458658fd3ec79d40
Van Huynh <vthuynh@cpp.edu>
2022-05-02 22:40:37 -0700

    navigation with WebViewNavigation

--- components/DesoLogin.js
+++ components/DesoLogin.js
@@ -1,15 +1,65 @@
-import React, { Component } from 'react';
-import { WebView } from 'react-native-webview';
+// import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
+import { WebView, WebViewNavigation } from 'react-native-webview';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { useNavigation } from '@react-navigation/native';
 
-export default class DesoLogin extends Component {
-  render() {
-    return (
+import Home from '../screens/Home';
+
+// const onNavigationStateChange = (navigationState: WebViewNavigation) => {
+//   const url = navigationState.url;
+
+//   // parseURLParams is a pseudo function.
+//   // Make sure to write your own function or install a package
+//   const params = parseURLParams(url);
+
+//   if (params.token) {
+//     // Save token for native requests & move to the next screen
+//   }
+// };
+
+// export default class DesoLogin extends Component {
+//   handleNavigationStateChange() {
+//     useNavigation.navigate("Home");
+//   }
+//   render() {
+//     return (
+//       <SafeAreaView style={{ flex: 1 }}>
+//         <WebView
+//           source={{
+//             uri: 'https://identity.deso.org/derive?callback=auth://derive'
+//           }}
+//           // onNavigationStateChange={handleNavigationStateChange()}
+//           style={{ marginTop: 20 }}
+//         />
+//       </SafeAreaView>
+//     );
+//   }
+// }
+
+const DesoLogin = () =>{
+  const [signIn, setSignIn] = useState(false)
+  const navigation = useNavigation();
+
+  const handleNavigationStateChange = () => {
+    navigation.navigate("Home");
+  }
+
+  useEffect(() => {
+    setSignIn(true);
+    handleNavigationStateChange();
+  },[])
+
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
       <WebView
         source={{
           uri: 'https://identity.deso.org/derive?callback=auth://derive'
         }}
+        // onNavigationStateChange={handleNavigationStateChange()}
         style={{ marginTop: 20 }}
       />
-    );
-  }
+    </SafeAreaView>
+  );
 }
+export default DesoLogin;
