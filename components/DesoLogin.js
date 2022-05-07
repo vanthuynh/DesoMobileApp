e0d9c6d02d3d8ae6dd3888f277b9d34bf859cc1f
Van Huynh <vthuynh@cpp.edu>
2022-05-06 19:18:58 -0700

    Remove unneeded code for using Deso Identity

--- components/DesoLogin.js
+++ components/DesoLogin.js
@@ -7,144 +7,49 @@ import { useNavigation } from '@react-navigation/native';
 import Home from '../screens/Home';
 import axios from 'axios';
 
-// const onNavigationStateChange = (navigationState: WebViewNavigation) => {
-//   const url = navigationState.url;
-
-//   // parseURLParams is a pseudo function.
-//   // Make sure to write your own function or install a package
-//   const params = parseURLParams(url);
-
-//   if (params.token) {
-//     // Save token for native requests & move to the next screen
-//   }
-// };
-function parseURLParams(url) {
-
-  return result;
-}
+// function parseURLParams(url) {
+//   return result;
+// }
 const DesoLogin = () =>{
   const axios = require('axios');
   const navigation = useNavigation();
-  // const webView = WebView();
   const [signIn, setSignIn] = useState(false)
   const [userProfile, setUserProfile] = useState(null)
-
-  function onMessage(data) {
-    alert(data.nativeEvent.data);
-  }
-
-  // const handleLogin = (e) => {
-  //   const data = webView.getUrl(e);
-  //   setUserProfile(data)
-  // }
 
   const handleNavigationStateChange = () => {
     navigation.navigate("Home", {userProfile});
   }
 
-  const onNavigationStateChange = (navigationState : WebViewNavigation) => {
-    const url = navigationState.url;
-
-    // const params = parseURLParams(url);
-    axios.get('/auth://derive')
-    .then(response => {
-      const account = response.data;
-      setUserProfile({account});
-    })
-    .catch((error)=> {
-      if (error.response) {
-        // The request was made and the server responded with a status code
-        // that falls out of the range of 2xx
-        console.log(error.response.data);
-        console.log(error.response.status);
-        console.log(error.response.headers);
-      } else if (error.request) {
-        // The request was made but no response was received
-        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-        // http.ClientRequest in node.js
-        console.log(error.request);
-      } else {
-        // Something happened in setting up the request that triggered an Error
-        console.log('Error', error.message);
+  const onNavigationStateChange = async (navigationState) => {
+    const { url } = navigationState;
+    if (url.includes("derivedSeedHex")) {
+      const payload = url.split("?")[1];
+      try {
+        let obj = payload.split("&").reduce(function(prev, curr, i, arr) {
+            var p = curr.split("=");
+            prev[decodeURIComponent(p[0])] = decodeURIComponent(p[1]);
+            return prev;
+        }, {});
+        setUserProfile(obj);
+        console.log(userProfile);
+        handleNavigationStateChange();
+      } catch(error) {
+        console.error(error);
       }
-    })
-    if (userProfile!==url) {
-      console.log('**********************************************');
-      console.log(url);
-      console.log('**********************************************');
-      setUserProfile(url);
     }
-    axios.get('/auth://derive')
-    .then(response => {
-      const account = response.data;
-      setUserProfile({account});
-    })
-    .catch((error)=> {
-      if (error.response) {
-        // The request was made and the server responded with a status code
-        // that falls out of the range of 2xx
-        console.log(error.response.data);
-        console.log(error.response.status);
-        console.log(error.response.headers);
-      } else if (error.request) {
-        // The request was made but no response was received
-        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-        // http.ClientRequest in node.js
-        console.log(error.request);
-      } else {
-        // Something happened in setting up the request that triggered an Error
-        console.log('Error', error.message);
-      }
-    })
   };
 
   // update userProfile and pass on to Home screen
-  useEffect(() => {
-    setSignIn(true);
-    axios.get('/auth://derive')
-    .then(response => {
-      const account = response.data;
-      setUserProfile({account});
-    })
-    .catch((error)=> {
-      if (error.response) {
-        // The request was made and the server responded with a status code
-        // that falls out of the range of 2xx
-        console.log(error.response.data);
-        console.log(error.response.status);
-        console.log(error.response.headers);
-      } else if (error.request) {
-        // The request was made but no response was received
-        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-        // http.ClientRequest in node.js
-        console.log(error.request);
-      } else {
-        // Something happened in setting up the request that triggered an Error
-        console.log('Error', error.message);
-      }
-  })
-    if (userProfile) {
-      // var access_token = new URLSearchParams(userProfile.search).get('derivedSeedHex');
-      console.log('+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+')
-      // console.log(access_token);
-      console.log(userProfile);
-      console.log('+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+')
-      // handleNavigationStateChange();
-    }
-  },[userProfile])
+  // useEffect(() => {
+  //   setSignIn(true);
+  // },[userProfile])
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <WebView
         source={{
-          // uri: 'https://identity.deso.org/derive?callback=auth://derive?webview=true'
-          // uri: 'https://identity.deso.org/derive?callback=...?'
-          // uri: 'https://identity.deso.org/derive?callback=auth://derive'
           uri: 'https://identity.deso.org/derive?callback=https://node.deso.org'
-          // uri: 'https://identity.deso.org/derive?webview=true'
         }}
-        onMessage={onMessage}
-        injectJavaScript={onNavigationStateChange}
         onNavigationStateChange={onNavigationStateChange}
         javaScriptEnabled
         style={{ marginTop: 20 }}
