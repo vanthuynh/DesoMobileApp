812d837469ae774ae8a9eb11015cf4eea820aa2c
Van Huynh <vthuynh@cpp.edu>
2022-05-06 19:06:13 -0700

    commit before add changed from Deric's help

--- components/DesoLogin.js
+++ components/DesoLogin.js
@@ -5,6 +5,7 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 import { useNavigation } from '@react-navigation/native';
 
 import Home from '../screens/Home';
+import axios from 'axios';
 
 // const onNavigationStateChange = (navigationState: WebViewNavigation) => {
 //   const url = navigationState.url;
@@ -22,6 +23,7 @@ function parseURLParams(url) {
   return result;
 }
 const DesoLogin = () =>{
+  const axios = require('axios');
   const navigation = useNavigation();
   // const webView = WebView();
   const [signIn, setSignIn] = useState(false)
@@ -44,18 +46,83 @@ const DesoLogin = () =>{
     const url = navigationState.url;
 
     // const params = parseURLParams(url);
-
+    axios.get('/auth://derive')
+    .then(response => {
+      const account = response.data;
+      setUserProfile({account});
+    })
+    .catch((error)=> {
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+      } else if (error.request) {
+        // The request was made but no response was received
+        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+        // http.ClientRequest in node.js
+        console.log(error.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log('Error', error.message);
+      }
+    })
     if (userProfile!==url) {
       console.log('**********************************************');
       console.log(url);
       console.log('**********************************************');
       setUserProfile(url);
     }
+    axios.get('/auth://derive')
+    .then(response => {
+      const account = response.data;
+      setUserProfile({account});
+    })
+    .catch((error)=> {
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+      } else if (error.request) {
+        // The request was made but no response was received
+        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+        // http.ClientRequest in node.js
+        console.log(error.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log('Error', error.message);
+      }
+    })
   };
 
   // update userProfile and pass on to Home screen
   useEffect(() => {
     setSignIn(true);
+    axios.get('/auth://derive')
+    .then(response => {
+      const account = response.data;
+      setUserProfile({account});
+    })
+    .catch((error)=> {
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+      } else if (error.request) {
+        // The request was made but no response was received
+        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+        // http.ClientRequest in node.js
+        console.log(error.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log('Error', error.message);
+      }
+  })
     if (userProfile) {
       // var access_token = new URLSearchParams(userProfile.search).get('derivedSeedHex');
       console.log('+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+')
@@ -71,8 +138,9 @@ const DesoLogin = () =>{
       <WebView
         source={{
           // uri: 'https://identity.deso.org/derive?callback=auth://derive?webview=true'
-          uri: 'https://identity.deso.org/derive?callback=...?'
+          // uri: 'https://identity.deso.org/derive?callback=...?'
           // uri: 'https://identity.deso.org/derive?callback=auth://derive'
+          uri: 'https://identity.deso.org/derive?callback=https://node.deso.org'
           // uri: 'https://identity.deso.org/derive?webview=true'
         }}
         onMessage={onMessage}
