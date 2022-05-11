05ad8ac21111bbcd28df1791804f116d51d90d0b
Dexter Nguyen <dexterious56@gmail.com>
2022-05-11 09:57:47 -0700

    changes

--- constants/theme.js
+++ constants/theme.js
@@ -15,20 +15,20 @@ export const SIZES = {
   large: 18,
   extraLarge: 24,
   radius: 30,
-    padding: 10,
-    padding2: 12,
+  padding: 10,
+  padding2: 12,
 
-    // font sizes
-    largeTitle: 50,
-    h1: 30,
-    h2: 22,
-    h3: 20,
-    h4: 18,
-    body1: 30,
-    body2: 20,
-    body3: 16,
-    body4: 14,
-    body5: 12,
+  // font sizes
+  largeTitle: 50,
+  h1: 30,
+  h2: 22,
+  h3: 20,
+  h4: 18,
+  body1: 30,
+  body2: 20,
+  body3: 16,
+  body4: 14,
+  body5: 12,
 
     
 };
