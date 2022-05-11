4acaa0b19ad68706a72c84f54e8a765bef40613d
Van Huynh <vthuynh@cpp.edu>
2022-05-11 13:45:26 -0700

    first try connect with Backend in Python

--- screens/LogIn.js
+++ screens/LogIn.js
@@ -20,6 +20,32 @@ const LogIn = ({ navigation }) => {
 
   // const navigation = useNavigation();
 
+  useEffect(() => {
+    axios.get('http://192.168.12.194:5000/getHomeCoordinates/MAC')
+    .then(response => {
+      const res = response.data;
+      // setUserProfile({account});
+      console.log(res);
+    })
+    .catch((error)=> {
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+      } else if (error.request) {
+        // The request was made but no response was received
+        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+        // http.ClientRequest in node.js
+        console.log(error.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log('Error', error.message);
+      }
+    })
+  }, [])
+
   const handleSignUp = () => {
     return 0;
   }
